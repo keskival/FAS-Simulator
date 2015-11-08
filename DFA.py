a88701614957f7011bda2c0281d4d38439b65508
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2015-11-08 19:07:18 +0200

    Moore minimization.

--- DFA.py
+++ DFA.py
@@ -20,12 +20,50 @@ for item in outputJson:
         transitions[lastItem][item['type']] = 1
     lastItem = item['type']
 
+# Moore reduction here
+itemToKPartition = dict()
+kPartitionToItems = dict()
+
 f = open('dfa.dot', 'w')
 f.write("digraph G {\n")
 for fromItem in transitions.keys():
-    for toItem in transitions[fromItem].keys():
+    # We will try to do minimization here also.
+    toItems = transitions[fromItem].keys()
+    for toItem in toItems:
         f.write("  \"" + fromItem + "\"->\"" + toItem + "\";\n")
-
+    k = ":".join(sorted(toItems))
+    # All fromItems in the same kPartition are kEquivalent, because their outputs are the same.
+    itemToKPartition[fromItem] = k
+    if (not kPartitionToItems.has_key(k)):
+        kPartitionToItems[k] = []
+    kPartitionToItems[k].append(fromItem)
+print kPartitionToItems
 f.write("}\n")
 f.close()
 
+f_reduced = open('dfa_reduced.dot', 'w')
+f_reduced.write("digraph G {\n")
+
+transitions_reduced = dict()
+
+# Reducing. One iteration is enough.
+for fromItemRaw in transitions.keys():
+    fromItemK = itemToKPartition[fromItemRaw]
+    fromItem = ":".join(sorted(kPartitionToItems[fromItemK]))
+    print fromItem
+
+    toItemsRaw = transitions[fromItemRaw].keys()
+    toItems = dict()
+    for toItemRaw in toItemsRaw:
+        toItemK = itemToKPartition[toItemRaw]
+        toItemKey = ":".join(sorted(kPartitionToItems[toItemK]))
+        toItems[toItemKey] = 1
+    transitions_reduced[fromItem] = toItems.keys()
+
+for fromItem in transitions_reduced.keys():
+    # We will try to do minimization here also.
+    toItems = transitions_reduced[fromItem]
+    for toItem in toItems:
+        f_reduced.write("  \"" + fromItem + "\"->\"" + toItem + "\";\n")
+f_reduced.write("}\n")
+f_reduced.close()
