35c919e291ff0ee9180709fafab51357dea927d7
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-01-25 02:03:24 +0200

    Fixed script.

--- compile_audio_to_video.sh
+++ compile_audio_to_video.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ffmpeg -i sequence.wav -pix_fmt yuv420p -loop 1 -i ./output/*.png -vf fps=10 -vcodec mpeg4 -strict -2 -b:a 384k -acodec aac -c:a aac -shortest fas2.mp4
+ffmpeg -i sequence.wav -pix_fmt yuv420p -loop 1 -i "output/%5d.png" -vf fps=10 -vcodec mpeg4 -strict -2 -b:a 384k -acodec aac -c:a aac -shortest fas2.mp4
 
