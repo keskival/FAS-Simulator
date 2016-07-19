b6754fd8bf395b7c310892b87ccc3d928b0f5aae
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-07-19 20:11:31 +0300

    Added a less hostile test case.

---
+++ run_sparse.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+import simpy
+from modules.production_line import ProductionLine
+from modules.fas_instance import FASInstance
+
+from logger import Logger
+
+# Initializing
+env = simpy.Environment()
+
+logger = Logger(env)
+
+production_line = ProductionLine(env, logger)
+
+# Doing a couple of global repeats
+
+for i in range(0,20):
+
+    # First running through with one item.
+
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    env.run()
+    
+    # Then 2 items spaced a bit.
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    for j in range(0, 5):
+        env.step()
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    env.run()
+    
+    # Then 2 items spaced more.
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    for j in range(0, 8):
+        env.step()
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    env.run()
+    
+    # Then 2 items spaced less.
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    for j in range(0, 3):
+        env.step()
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    env.run()
+    
+    # Then 3 items spaced a bit.
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    for j in range(0, 5):
+        env.step()
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    for j in range(0, 5):
+        env.step()
+    
+    fas_instance = FASInstance(env, production_line)
+    fas_instance.spawn()
+    
+    env.run()
+
+print("Done.")
+
+f = open('output.json', 'w')
+f.write(logger.getLoglines())
+f.close()
