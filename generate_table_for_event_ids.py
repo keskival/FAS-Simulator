470a0bcfdf4668457286842782a05fbeb43b6a6c
Tero Keski-Valkama <tero.keski-valkama@neter.fi>
2022-01-02 16:34:55 +0100

    Added the event id table generation and the table to the README.

---
+++ generate_table_for_event_ids.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from modules.process.production_line import ProductionLine
+from modules.components.clock import Clock
+
+# Getting all event types to make an index out of them.
+production_line = ProductionLine(None, None, False)
+clock = Clock(None, None, False)
+all_event_types = clock.get_events() + production_line.get_events()
+
+print("Event id | Event name")
+print("--- | ---")
+for id, event in enumerate(all_event_types):
+    print(f"{id} | {event}")
+
+print("""\\begin{table}[!t]
+\\caption{Table of the Event Identifiers}
+\\label{eventids}
+\\centering
+\\begin{tabular}{|p{15mm}|p{15mm}|}
+\\hline
+Event id & Event name \\\\
+\\hline
+\\hline""")
+for id, event in enumerate(all_event_types):
+    event = event.replace("_", "\\_")
+    print(f"{id} & {event} \\\\")
+    print("\\hline")
+print("""\\end{tabular}
+\\end{table}""")
